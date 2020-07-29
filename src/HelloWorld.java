ae9b954de9f37a5416044e5b8bc2e71be1555cfc
java1234 <caofeng2012@126.com>
2020-07-29 21:46:54 +0800

    xxx

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * @author java1234_小锋
+ * @site www.java1234.com
+ * @company Java知识分享网
+ * @create 2020-07-29 21:46
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
